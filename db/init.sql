e94b13e0789c636c2e0bf9a3477b6aed2563e62a
Lysepten <tlstlsrbrb11@gmail.com>
2024-04-26 14:54:11 +0900

    유틸, initData 기반, JPA 기반, AppConfig 기반

---
+++ db/init.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS acc_app_2024_04__test;
+CREATE DATABASE acc_app_2024_04__test;
+USE acc_app_2024_04__test;
+
+# 개발 DB 생성
+DROP DATABASE IF EXISTS acc_app_2024_04__dev;
+CREATE DATABASE acc_app_2024_04__dev;
+USE acc_app_2024_04__dev;
