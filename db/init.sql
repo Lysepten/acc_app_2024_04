8ae9e93486150c7bac4825768b0dc2d55b7a1377
Lysepten <tlstlsrbrb11@gmail.com>
2024-05-09 13:49:38 +0900

    DB 수정

--- db/init.sql
+++ db/init.sql
@@ -8,7 +8,7 @@ DROP DATABASE IF EXISTS acc_app_2024_04__dev;
 CREATE DATABASE acc_app_2024_04__dev;
 USE acc_app_2024_04__dev;
 
--- SELECT order_item_id, pay_price, refund_price, wholesale_price, pg_fee
+-- SELECT order_item_id, pay_price, refund_price, wholesale_price, pg_fee,
 --     CASE
 -- 	WHEN pay_price = refund_price
 --     THEN 0
