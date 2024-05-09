d29de902d0dd52bb08f5345312cee7199dfd92d1
Lysepten <tlstlsrbrb11@gmail.com>
2024-05-09 14:39:05 +0900

    정산 데이터 목록 추가, RebateOrderItem 목록 구현, 건별정산

--- src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/service/RebateService.java
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/service/RebateService.java
@@ -1,5 +1,8 @@
 package com.koreait.exam.acc_app_2024_04.app.rebate.service;
 
+import com.koreait.exam.acc_app_2024_04.app.base.dto.RsData;
+import com.koreait.exam.acc_app_2024_04.app.cash.entity.CashLog;
+import com.koreait.exam.acc_app_2024_04.app.member.service.MemberService;
 import com.koreait.exam.acc_app_2024_04.app.order.entity.OrderItem;
 import com.koreait.exam.acc_app_2024_04.app.order.service.OrderService;
 import com.koreait.exam.acc_app_2024_04.app.rebate.entity.RebateOrderItem;
@@ -7,17 +10,22 @@ import com.koreait.exam.acc_app_2024_04.app.rebate.repository.RebateOrderItemRep
 import com.koreait.exam.acc_app_2024_04.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RebateService {
     private final OrderService orderService;
+    private final MemberService memberService;
     private final RebateOrderItemRepository rebateOrderItemRepository;
 
+    @Transactional
     public void makeDate(String yearMonth) {
         int monthEndDay = Ut.date.getEndDayOf(yearMonth);
 
@@ -27,7 +35,7 @@ public class RebateService {
         LocalDateTime toDate = Ut.date.parse(toDateStr);
 
         // 데이터 가져오기
-        List<OrderItem> orderItems = orderService.findAllByPayDateBetween(fromDate, toDate);
+        List<OrderItem> orderItems = orderService.findAllByPayDateBetweenOrderByIdAsc(fromDate, toDate);
 
         // 변환하기
         List<RebateOrderItem> rebateOrderItems = orderItems
@@ -39,6 +47,7 @@ public class RebateService {
         rebateOrderItems.forEach(this::makeRebateOrderItem);
     }
 
+    @Transactional
     public void makeRebateOrderItem(RebateOrderItem item) {
         RebateOrderItem oldRebateOrderItem = rebateOrderItemRepository.findByOrderItemId(item.getOrderItem().getId()).orElse(null);
 
@@ -51,5 +60,40 @@ public class RebateService {
 
     public RebateOrderItem toRebateOrderItem(OrderItem orderItem) {
         return new RebateOrderItem(orderItem);
+    }
+
+    public List<RebateOrderItem> findRebateOrderItemsByPayDateIn(String yearMonth) {
+        int monthEndDay = Ut.date.getEndDayOf(yearMonth);
+
+        String fromDateStr = yearMonth + "-01 00:00:00.000000";
+        String toDateStr = yearMonth + "-%02d 23:59:59.999999".formatted(monthEndDay);
+        LocalDateTime fromDate = Ut.date.parse(fromDateStr);
+        LocalDateTime toDate = Ut.date.parse(toDateStr);
+
+        return rebateOrderItemRepository.findAllByPayDateBetweenOrderByIdAsc(fromDate, toDate);
+    }
+
+    @Transactional
+    public RsData rebate(long orderItemId) {
+        RebateOrderItem rebateOrderItem = rebateOrderItemRepository.findByOrderItemId(orderItemId).get();
+
+        if (rebateOrderItem.isRebateAvailable() == false) {
+            return RsData.of("F-1", "정산을 할 수 없는 상태입니다.");
+        }
+
+        int calculateRebatePrice = rebateOrderItem.calculateRebatePrice();
+
+        RsData<Map<String, Object>> addCashRsData = memberService.addCash(rebateOrderItem.getProduct().getAuthor(), calculateRebatePrice, "정산__%d__지급__예치금".formatted(rebateOrderItem.getOrderItem().getId()));
+        CashLog cashLog = (CashLog) addCashRsData.getData().get("cashLog");
+
+        rebateOrderItem.setRebateDone(cashLog.getId());
+
+        return RsData.of(
+                "S-1",
+                "정산성공",
+                Ut.mapOf(
+                        "cashLogId", cashLog.getId()
+                )
+        );
     }
 }
